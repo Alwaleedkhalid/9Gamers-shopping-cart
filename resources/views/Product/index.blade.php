fdf9f970b792f20de65d6c8b7eab4cf198e45305
Alwaleed <alwaleed.alageel@gmail.com>
2020-09-08 01:37:48 +0300

    nav

--- resources/views/Product/index.blade.php
+++ resources/views/Product/index.blade.php
@@ -6,20 +6,20 @@
     @endif --}}
     <div class="container">
         <div class="row">
-        @foreach ($Products as $Product)
-            <div class="col-md-4">
-          <a href="{{route('cart.add', $Product->id)}}">
-            <div class="profile-card-2"><img src="{{$Product->image}}" class="card-img-top">
-              {{-- <div class="profile-name">{{$Product->title}}</div> --}}
-              <div class="profile-name">{{$Product->price}} SR</div>
-              <div class="profile-username"><i class="fas fa-shopping-cart"></i> Add To Cart</div>
-              </a>
-          </div>
-      </div>
-      @endforeach
-    
-    
-    
-        </div>
+                        @foreach ($Products as $Product)
+                            <div class="col-md-4">
+                        <a href="{{route('cart.add', $Product->id)}}">
+                            <div class="profile-card-2"><img src="{{$Product->image}}" class="card-img-top">
+                            {{-- <div class="profile-name">{{$Product->title}}</div> --}}
+                            <div class="profile-name">{{$Product->price}} SR</div>
+                            <div class="profile-username"><i class="fas fa-shopping-cart"></i> Add To Cart</div>
+                            </a>
+                        </div>
+                    </div>
+                    @endforeach
+                    
+                    
+                    
+                        </div>
 
 @endsection
